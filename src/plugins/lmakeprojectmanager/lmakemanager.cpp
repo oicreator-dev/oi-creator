dcccc39ec2af589d76089550c68a8b17c6a0c964
WAAutoMaton <a547673135@gmail.com>
2018-02-13 23:18:47 +0800

    新建LmakeManager

---
+++ src/plugins/lmakeprojectmanager/lmakemanager.cpp
@@ -0,0 +1,6 @@
+#include "lmakemanager.h"
+
+LmakeManager::LmakeManager()
+{
+
+}
---
+++ src/plugins/lmakeprojectmanager/lmakemanager.h
@@ -0,0 +1,11 @@
+#ifndef LMAKEMANAGER_H
+#define LMAKEMANAGER_H
+
+
+class LmakeManager
+{
+public:
+    LmakeManager();
+};
+
+#endif // LMAKEMANAGER_H
